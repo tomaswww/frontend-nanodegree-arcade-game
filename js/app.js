98b4b7877c63599f7deda920fe89ac2c0389dcf2
Tomas Wingord <twingord@gmail.com>
2018-10-13 16:15:46 -0300

    added dificulty function and initialization

--- js/app.js
+++ js/app.js
@@ -12,33 +12,33 @@ class Enemy {
     this.height = 80;
     this.width = 65;
   }
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
-update(dt) {
-  this.x += dt * this.speed;
-  // You should multiply any movement by the dt parameter
-  // which will ensure the game runs at the same speed for
-  // all computers.
-  //looping the enemies
-  if (this.x > 500) {
-    this.x = -100;
-  }
-  //checking for collision
-  if (this.x === player.x && this.y === player.y) {
-    player.x = 202;
-    player.y = 322;
-    //takes life and ends game when out of lifes
-    player.lifes--;
-    start.displayLifes();
-    if (player.lifes === 0) {
-      start.modal(start.endModal);
+  // Update the enemy's position, required method for game
+  // Parameter: dt, a time delta between ticks
+  update(dt) {
+    this.x += dt * this.speed;
+    // You should multiply any movement by the dt parameter
+    // which will ensure the game runs at the same speed for
+    // all computers.
+    //looping the enemies
+    if (this.x > 500) {
+      this.x = -100;
+    }
+    //checking for collision
+    if (this.x === player.x && this.y === player.y) {
+      player.x = 202;
+      player.y = 322;
+      //takes life and ends game when out of lifes
+      player.lifes--;
+      start.displayLifes();
+      if (player.lifes === 0) {
+        start.modal(start.endModal);
+      }
     }
   }
-}
-// Draw the enemy on the screen, required method for game
-render() {
-  ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-}
+  // Draw the enemy on the screen, required method for game
+  render() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+  }
 };
 
 // Now write your own player class
@@ -95,58 +95,71 @@ class Player {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
   };
 };
-  // Now instantiate your objects.
-  // Update score and level in board
-  function board(level,score) {
-    document.querySelector(".score-value").textContent = score;
-    document.querySelector(".level-value").textContent = level;
+// Now instantiate your objects.
+// Update score and level in board
+function board(level, score) {
+  document.querySelector(".score-value").textContent = score;
+  document.querySelector(".level-value").textContent = level;
+};
+// Place the player object in a variable called player
+const start = {
+  initialize: function() {
+    this.allEnemies = [];
+    this.nextRow = 0;
+    this.create();
+    this.displayLifes();
+  },
+  // Place all enemy objects in an array called allEnemies
+  create: function() {
+    for (let row of this.rowPosition) {
+      let val = this.newVals();
+      let enemy = new Enemy(val.Xo, row, val.speed);
+      this.allEnemies.push(enemy);
+    }
+  },
+  displayLifes: function() {
+    let lifes = document.querySelector(".lifes");
+    lifes.innerHTML = "";
+    for (let i = 0; i < player.lifes; i++) {
+      var newContent = document.createTextNode("+");
+      lifes.appendChild(newContent);
+    };
+  },
+  newVals()
+  //function used to give x axis value and speed.
+  {
+    let speed = Math.floor(Math.random() * 100 + 5 * player.level);
+    let Xo = -Math.floor(Math.random() * 500 + 100);
+    return {
+      speed: speed,
+      Xo: Xo
+    }
+  }
+  setDifficult: function() {
+    let value = this.newVals();
+    let row = this.rowPosition[this.nextRow];
+    this.nextRow++;
+    if (this.nextRow == 3) {
+      this.nextRow = 0;
+    }
+    if (this.allEnemies.length == 6) {
+      this.allEnemies.splice(0, 1);
+    }
+    let enemy = new Enemy(values.initX, row, values.speed);
+    this.allEnemies.push(enemy);
+  }
+}
+
+start.initialize();
+// This listens for key presses and sends the keys to your
+// Player.handleInput() method. You don't need to modify this.
+document.addEventListener('keyup', function(e) {
+  var allowedKeys = {
+    37: 'left',
+    38: 'up',
+    39: 'right',
+    40: 'down'
   };
-  // Place the player object in a variable called player
-  const start = {
-      init: function() {
-        this.allEnemies = [];
-        this.nextRow = 0;
-        this.create();
-        this.displayLifes();
-      },
-      // Place all enemy objects in an array called allEnemies
-      create: function() {
-        for (let row of this.rowPosition) {
-          let val = this.newVals();
-          let enemy = new Enemy(val.Xo, row, val.speed);
-          this.allEnemies.push(enemy);
-        }
-      },
-      displayLifes: function() {
-        let lifes = document.querySelector(".lifes");
-        lifes.innerHTML = "";
-        for (let i = 0; i < player.lifes; i++) {
-          var newContent = document.createTextNode("+");
-          lifes.appendChild(newContent);
-        };
-},
-        newVals()
-        //function used to give x axis value and speed.
-        {
-          let speed = Math.floor(Math.random() * 100 + 5 * player.level);
-          let Xo = -Math.floor(Math.random() * 500 + 100);
-          return {
-            speed: speed,
-            Xo: Xo
-          }
-        }
-      }
 
-
-      // This listens for key presses and sends the keys to your
-      // Player.handleInput() method. You don't need to modify this.
-      document.addEventListener('keyup', function(e) {
-        var allowedKeys = {
-          37: 'left',
-          38: 'up',
-          39: 'right',
-          40: 'down'
-        };
-
-        player.handleInput(allowedKeys[e.keyCode]);
-      });
+  player.handleInput(allowedKeys[e.keyCode]);
+});
